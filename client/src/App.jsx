afcd30fefebe3ebe22d9ad8dc226a3c75eaff65c
Yuvraj Mehta <yuvraj.mehta532@gmail.com>
2025-06-27 00:42:10 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-gray-900 min-h-screen flex items-center justify-center'>
+      <div className='text-red-400'>Hello</div>
+    </div>
+  )
+}
+
+export default App
